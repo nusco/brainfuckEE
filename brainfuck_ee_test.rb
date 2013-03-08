ad35a152fb62e6681b3acf8b2fc1d9945c68810d
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2013-03-09 00:52:44 +0100

    Add comments to 'Hello World'
    Comment 'Hello World'

--- brainfuck_ee_test.rb
+++ brainfuck_ee_test.rb
@@ -14,27 +14,27 @@ class BrainfuckEETest < Test::Unit::TestCase
   
   def test_hello_world
     code = "
-      +++++ +++++             initialize counter (cell #0) to 10
-      [                       use loop to set the next four cells to 70/100/30/10
-          > +++++ ++              add  7 to cell #1
-          > +++++ +++++           add 10 to cell #2 
-          > +++                   add  3 to cell #3
-          > +                     add  1 to cell #4
-          <<<< -                  decrement counter (cell #0)
-      ]                   
-      > ++ .                  print 'H'
-      > + .                   print 'e'
-      +++++ ++ .              print 'l'
-      .                       print 'l'
-      +++ .                   print 'o'
-      > ++ .                  print ' '
-      << +++++ +++++ +++++ .  print 'W'
-      > .                     print 'o'
-      +++ .                   print 'r'
-      ----- - .               print 'l'
-      ----- --- .             print 'd'
-      > + .                   print '!'
-      > .                     print '\n'"
+       +++++ +++++             initialize counter (cell #0) to 10
+       [                       use loop to set the next four cells to 70/100/30/10
+           > +++++ ++              add  7 to cell #1
+           > +++++ +++++           add 10 to cell #2 
+           > +++                   add  3 to cell #3
+           > +                     add  1 to cell #4
+           <<<< -                  decrement counter (cell #0)
+       ]                   
+       > ++ .                  print 'H'
+       > + .                   print 'e'
+       +++++ ++ .              print 'l'
+       .                       print 'l'
+       +++ .                   print 'o'
+       > ++ .                  print ' '
+       << +++++ +++++ +++++ .  print 'W'
+       > .                     print 'o'
+       +++ .                   print 'r'
+       ----- - .               print 'l'
+       ----- --- .             print 'd'
+       > + .                   print '!'
+       > .                     print '\n'"
     brainfuckEE(code)
     assert_equal "Hello World!\n", @output.string
   end
